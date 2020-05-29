9529c01fc750fd1f46d60806bb3d61d9e81e8e00
Julia Lojek <julialojek@gmail.com>
2020-05-29 12:17:15 +0200

    01-drumkit fixed

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,21 +1,10 @@
 var keys = document.querySelector(".keys");
-var SOUND_IDS = {
-    KeyA: "65",
-    KeyS: "83",
-    KeyD: "68",
-    KeyF: "70",
-    KeyG: "71",
-    KeyH: "72",
-    KeyJ: "74",
-    KeyK: "75",
-    KeyL: "76"
-};
 function play(event) {
-    var activeElKeyAttr = SOUND_IDS[event.code];
-    if (!activeElKeyAttr) {
+    var activeElKeyAttr = event.keyCode.toString();
+    var activeEl = keys.querySelector("[data-key=\"" + activeElKeyAttr + "\"]");
+    if (!activeEl) {
         return;
     }
-    var activeEl = keys.querySelector("[data-key=\"" + activeElKeyAttr + "\"]");
     activeEl.classList.add("playing");
     setTimeout(function () { return activeEl.classList.remove("playing"); }, 200);
     var activeAudioEl = document.querySelector("audio[data-key=\"" + activeElKeyAttr + "\"]");
--- 01 - JavaScript Drum Kit/script.ts
+++ 01 - JavaScript Drum Kit/script.ts
@@ -1,29 +1,19 @@
 const keys = document.querySelector(".keys")! as HTMLDivElement;
 
-const SOUND_IDS = {
-  KeyA: "65",
-  KeyS: "83",
-  KeyD: "68",
-  KeyF: "70",
-  KeyG: "71",
-  KeyH: "72",
-  KeyJ: "74",
-  KeyK: "75",
-  KeyL: "76",
-}
-
 function play(event: KeyboardEvent) {
-  const activeElKeyAttr: string = SOUND_IDS[event.code];
+  const activeElKeyAttr: string = event.keyCode.toString();
 
-  if (!activeElKeyAttr) {
+  const activeEl = keys.querySelector(`[data-key="${activeElKeyAttr}"]`);
+  if (!activeEl) {
     return;
   }
-  const activeEl = keys.querySelector(`[data-key="${activeElKeyAttr}"]`)
   activeEl.classList.add("playing");
   setTimeout(() => activeEl.classList.remove("playing"), 200);
 
-  const activeAudioEl = document.querySelector(`audio[data-key="${activeElKeyAttr}"]`) as HTMLAudioElement;
+  const activeAudioEl = document.querySelector(
+    `audio[data-key="${activeElKeyAttr}"]`
+  ) as HTMLAudioElement;
   activeAudioEl.play();
 }
 
-window.addEventListener("keydown", (event) => play(event))
+window.addEventListener("keydown", (event) => play(event));
